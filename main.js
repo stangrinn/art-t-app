4ba21bbfebf25a069aac2505a3c30a1f595006d4
Stan Grinshpun <s.hrynshpun@asbis.com>
2024-09-04 22:25:08 +0300

    Draft: Start video playback on recognize target picture - Added new libs

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-import * as THREE from 'three';
-import { MindARThree } from 'mindar-image-three';
+import {loadVideo} from "./libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
-    // initialize MindAR 
-    const mindarThree = new MindARThree({
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: 'assets/targets/mother-day.mind',
+      imageTargetSrc: './assets/targets/sintel.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    // create AR object
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
-    const plane = new THREE.Mesh(geometry, material);
+    const video = await loadVideo("./assets/videos/sintel.mp4");
+    // const video = await loadVideo("./assets/videos/MDanimation.mov");
+    
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 204/480), new THREE.MeshBasicMaterial({map: new THREE.VideoTexture(video)}));
 
-    // create anchor
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
 
-    // start AR
+    anchor.onTargetFound = () => video.play();
+    anchor.onTargetLost = () => video.pause();
+    
+    video.addEventListener( 'play', () => {
+      video.currentTime = 6;
+    });
+
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
